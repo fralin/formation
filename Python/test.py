8994d4f3e822489279ddfa2edb13c6ae47263eb8
fralin <francois.alin@univ-reims.fr>
2023-03-04 06:34:12 -1000

    Delete Python directory

--- Python/test.py
+++
@@ -1,9 +0,0 @@
-from puissance import calc_puissance
-import unittest
-
-class TestPuissance(unittest.TestCase):
-    def test_puissance(self):
-        self.assertEqual(calc_puissance(2,2), 4)
-        
-if __name__ == "__main__":        
-    unittest.main()
